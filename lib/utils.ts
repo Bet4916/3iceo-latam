25b239670104362e795594e46bd152c621c34a0e
Bet4916 <bet4916@gmail.com>
2026-06-13 11:16:50 -0600

    upd

--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-/** Combina clases de Tailwind evitando duplicados y conflictos */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
